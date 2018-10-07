d32ba9b6715b597d6feb6ecda3c251a513b4bd39
Noman Hussain <nomanghanchi_11@yahoo.com>
2018-10-08 02:04:19 +0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Noman Hussain on 03/01/2018.
+//  Copyright © 2018 Noman Hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
